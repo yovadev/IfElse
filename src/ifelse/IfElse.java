521914942e4c22fc2cc01fdfd2e7e8e7becfa87d
Yova <10067268+yovadev@users.noreply.github.com>
2018-10-13 10:39:54 -0600

    Initial commit

---
+++ src/ifelse/IfElse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifelse;
+
+/**
+ *
+ * @author Yova
+ */
+public class IfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int variable = 6;
+        if (variable >= 6) {
+            System.out.println("se cumple la condición");
+        } else {
+            System.out.println("no se cumple la condición");
+        }
+    }
+    
+}
